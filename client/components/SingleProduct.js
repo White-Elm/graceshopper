76adf012d3eae40c26b9bb28fa0a01fa82261317
Jamie Ha <jamieha8794@gmail.com>
2021-10-14 20:19:24 -0400

    added single product and store component

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import SimilarProducts from './SimilarProducts'
+
+/*
+class SingleProduct extends Component{
+    constructor(props){
+        super(props)
+        const {products, match: {params: {id} }} = this.props
+        const product = products.find(product =>{ product.id === id*1})
+        const productid = product.id
+        this.state = {
+            productQty: 0,
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    componentDidUpdate(prevProps){
+        if(!prevProps.product.id && productid){
+            this.setState({
+                productQty: productQty,
+            })
+        }
+    }
+    
+    onSubmit(event){
+        if(productQty === ''){
+            productQty = 0;
+        }
+        this.props.addToCart(productid, productQty)
+    }
+    onChange(event){
+        const change = {};
+        change[event.target.name] = event.target.value;
+        this.setState(change)
+    }
+    render(){
+        
+        return(
+            <div>
+                <img src={product.imageUrl ? product.imageUrl : ''}/>
+                <div>
+                    {product.name}
+                    {product.price}
+                    {product.description}
+                </div>
+                <form>
+                    <label>Qty.</label>
+                    <input value={initalQty} name='qty' onChange={onChange}/>
+                    <button>Add to Cart</button>
+                </form>
+                <SimilarProducts/>
+            </div>
+        )
+    }
+
+}
+
+
+const mapStateToProps = (state) =>{
+    return state
+}
+const mapDispatchToProps = (dispatch, {history}) =>{
+    return{
+        addToCart: (productid, productQty) =>{
+            dispatch(addToCart(productid, productQty))
+        }
+    }
+}
+export default connect (mapStateToProps)(SingleProduct)
+
+*/
+
+class SingleProduct extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        const {products, match: {params: {id} }} = this.props
+        return (
+            <div>
+                <ul>
+                {products.map(product =>{
+                    <li>
+                        {product.name}
+                    </li>
+                })}
+                </ul>
+            </div>
+
+        )
+    }
+
+}
+
+const mapStateToProps = (state) =>{
+    return state
+}
+export default connect (mapStateToProps)(SingleProduct)
--- client/store/index.js
+++ client/store/index.js
@@ -3,6 +3,17 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import axios from 'axios'
+
+
+const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+
+const productReducers = (state = [], action) =>{
+  if(action.type === LOAD_PRODUCTS){
+    state = action.students;
+  }
+  return state;
+}
 
 const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
@@ -10,5 +21,24 @@ const middleware = composeWithDevTools(
 )
 const store = createStore(reducer, middleware)
 
+
+
+const _loadProducts = (products) =>{
+    return {
+        type: LOAD_PRODUCTS,
+        products,
+    }
+
+}
+
+const loadProducts = () =>{
+    return async (dispatch) =>{
+        const products = (await axios.get('/api/products')).data
+        dispatch(_loadProducts(products))
+    }
+}
+
+
 export default store
 export * from './auth'
+export {loadProducts}
